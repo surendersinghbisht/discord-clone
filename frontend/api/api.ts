d83b6d80746e8b8c77280d8654ae9f4bf8947a63
Surender singh bisht <102962504+surendersinghbisht@users.noreply.github.com>
2025-04-14 02:33:56 +0530

    Update api.ts

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import { io } from "socket.io-client";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://discord-clone-api-nu.vercel.app/api", withCredentials: true
+    baseURL: "https://discord-clone-nine-blond.vercel.app/api", withCredentials: true
 })
 
 
 
-const SOCKET_URL = "https://discord-clone-api-nu.vercel.app/api"; 
+const SOCKET_URL = "https://discord-clone-nine-blond.vercel.app/api"; 
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
